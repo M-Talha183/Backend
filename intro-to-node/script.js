4e4daa67576bd5b6e354713bd5656af82a1abe61
Muhammad361738 <ullahehsan016@gmail.com>
2025-08-23 22:55:50 +0500

    update into-to-node folder with some example

--- intro-to-node/script.js
+++ intro-to-node/script.js
@@ -1,6 +1,28 @@
 const fs = require("fs")
+// ************** Create File **************
 
-fs.writeFile("talha.txt","Hello World In Node Js ", function (err){
+
+let myFile = fs.writeFile("talha.txt","Hello World In Node Js ", function (err){
     if (err) console.log(err);
     else console.log("File Created Successfully");
 })
+// *************** Append File (update file) ***************
+
+
+fs.appendFile("talha.txt","zain and talha and are leraning backend at same time ", ((err)=>{
+    if(err) console.log("ERRor ", err)
+    else console.log("File Updated Successfully")
+}))
+
+//  ************** Rename File ***************
+
+fs.rename("talha.txt","zain.txt", (err)=>{
+    if(err) console.log("Error ", err)
+    else console.log("File Renamed Successfully")
+})
+
+// ***************** Delete **********************
+fs.unlink("talha.txt",((err)=>{
+    if(err) console.log("Error ", err)
+    else console.log("File Deleted Successfully")
+}))
